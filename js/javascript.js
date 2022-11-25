f675aab004ee62f1bca2405ef89f6fadd5ddf442
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-25 16:59:34 +0100

    readme fatto + javascript fatto con errore

---
+++ js/javascript.js
@@ -0,0 +1,35 @@
+// 1. Dare la possibilità di inserire il numero di klometri
+const prezzo_km = parseInt(prompt("quanti km devi fare?"));
+console.log(prezzo_km)
+
+// 2. Dare la possibilità di inserire età
+const eta = parseInt(prompt("quanti anni hai?"));
+console.log(eta)
+
+// 3. Definisco il rezzo del biglietto senza sconti moltiplicando 0.20€ per il numero di km
+const prezzo_intero = prezzo_km * 0.2;
+console.log(prezzo_intero)
+
+// 4. determinare il prezzo finale sottraendo lo sconto età al prezzo intero
+//     4.1 calcolare lo sconto per i minori di 18 anni - 20%
+const prezzo_s_minori = prezzo_intero * 20 / 100;
+
+//     4.2 calcolare sconto per gli over 65 - 40%
+const prezzo_s_over65 = prezzo_intero * 40 /100;
+
+// 5. Determinare le condizioni affinchè venga calcolato un prezzo piuttosto che un altro
+//     5.1 se ho meno di 18 anni
+//     5.2 se ho 65 anni o più
+//     5.3 se non ho ditirro allo sconto (18-64)
+if eta < 18 {
+    let prezzo_finito = prezzo_intero - prezzo_s_minori
+}
+
+else if eta >= 65 {
+    let prezzo_finito = prezzo_intero - Prezzo_s_over65
+}
+
+else {
+    let prezzo_finito = prezzo_intero
+}
+// 6. Display del pezzo finito
---
+++ readme.txt
@@ -0,0 +1,13 @@
+PROBLEMA: Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65. L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+1. Dare la possibilità di inserire il numero di klometri
+2. Dare la possibilità di inserire età
+3. Definisco il rezzo del biglietto senza sconti moltiplicando 0.20€ per il numero di km
+4. determinare il prezzo finale sottraendo lo sconto età al prezzo intero
+    4.1 calcolare lo sconto per i minori di 18 anni - 20%
+    4.2 calcolare sconto per gli over 65 - 40%
+5. Determinare le condizioni affinchè venga calcolato un prezzo piuttosto che un altro
+    5.1 se ho meno di 18 anni
+    5.2 se ho 65 anni o più
+    5.3 se non ho ditirro allo sconto (18-64)
+6. Display del pezzo finito
